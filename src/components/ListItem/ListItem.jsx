db109caf486f007a916b11d083372a5fbeb08117
T Mark <tmk1.tran@gmail.com>
2023-09-21 16:36:12 -0500

    buy operational

--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -1,13 +1,19 @@
 export default function ListItem({ item, updateItem, handleDelete}) {
-    // const itemClassName = item.purchased ? 'purchased' : '';
 
   return ( 
     <div className="card">
       <p>{item.name}</p>
       <p>{item.quantity} {item.unit}</p>
       <br />
-      <button className="buyBtn" onClick={() => updateItem(item.id)}>Buy</button> 
-      <button className="deleteBtn" onClick={() => handleDelete(item.id)}>Remove</button>
+      <div>
+      {item.purchased ? (
+        <span>Purchased</span>
+      ) : (
+        <div>
+          <button className="buyBtn" onClick={() => updateItem(item.id)}>Buy</button>
+          <button className="deleteBtn" onClick={() => handleDelete(item.id)}>Remove</button>
+        </div>)}
+    </div>
     </div>
   );
 }
