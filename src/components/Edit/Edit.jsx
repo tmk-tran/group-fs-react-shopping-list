db5b5a8a6d68a72dac7ec72df463abd60e35b69c
T Mark <tmk1.tran@gmail.com>
2023-09-24 23:58:08 -0500

    update, edit

--- src/components/Edit/Edit.jsx
+++ src/components/Edit/Edit.jsx
@@ -18,34 +18,3 @@ export default function Edit({current, list, setList}) {
       </>
     );
   }
-
-
-{/*
-
-function handleEdit(id) {
-  setUpdateState(id)
-}
-
-under list.map:
-
-updateState === current.id? <EditItem current={current} items={items} setItems={setItems} /> :
-
-
-function Edit(current, list, setItems) {
-  function handleInput(e) {
-const newList = list.map)div => (
-  div.id === current.id ? {...div, [e.target.name] : e.target.value} : div
-)
-setList(newList)
-}
-  return(
-    <>
-    <input type="text" name ="item" value={current.item} onChange={handleInput} />
-    <br/>
-    <input type="int" name="qty" value={current.qty} onChange={handleInput} />
-    <br/>
-    <button type="submit" >Update<button/>
-    <>
-  )
-}
-*/}
